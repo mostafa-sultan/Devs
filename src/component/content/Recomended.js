1ba84eac0e38cd49c5392e900beb4236f5e61e0a
mostafa-sultan <mostafasoltan82@gmail.com>
2022-02-16 03:11:02 +0200

    mk

--- src/component/content/Recomended.js
+++ src/component/content/Recomended.js
@@ -1,18 +1,17 @@
-import React from 'react';
-
+import React from 'react'; 
 const Recomended = () => {
     return (
         <>
             
             <h4 className="fst-italic">Recomended</h4>
                                 <ol className="list-unstyled mb-0"> 
-                                    <li><a href="#">advanced ui react native</a></li>
-                                    <li><a href="#">simple movies Recommender system</a></li>
-                                    <li><a href="#">Dom Vs Bom</a></li>
-                                    <li><a href="#">oop</a></li>
-                                    <li><a href="#">python</a></li>
-                                    <li><a href="#">Express.js</a></li>
-                                    <li><a href="#">mongodb</a></li>
+                                    <li><a href="#/tutorial/reactnativeui.md">advanced ui react native</a></li>
+                                    <li><a href="#/tutorial/Recommendersystem.md">simple movies Recommender system</a></li>
+                                    <li><a href="#/tutorial/domvsbom.md">Dom Vs Bom</a></li>
+                                    <li><a href="#/tutorial/oop.md">oop</a></li>
+                                    <li><a href="#/tutorial/python.md">python</a></li>
+                                    <li><a href="#/tutorial/Expressjs.md">Express.js</a></li>
+                                    <li><a href="#/tutorial/mongodb.md">mongodb</a></li>
                                 </ol>
                            
         </>
