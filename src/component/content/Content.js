98ce3a24b5ab4aa30d76c81bce024220b6a29b4e
mostafa-sultan <mostafasoltan82@gmail.com>
2022-02-16 06:59:04 +0200

    jj

--- src/component/content/Content.js
+++ src/component/content/Content.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from 'react'
 import './Content.css';
 import ReactMarkdown from 'react-markdown'
 import axios from 'axios';
-import { useParams } from 'react-router-dom'
-  
+import { useNavigate } from 'react-router-dom'
+
 // ![image info](https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg)
 const Content = (props) => {
+  const history = useNavigate();
+
   const [data, setData] = useState('# loading ...');
-  const [content,] = useState(useParams().content);
+  const [content, setContent] = useState(window.location.hash.split("/")[2]);
   // console.log(useParams().content);
 
   useEffect(() => {
-    const contentLink = "https://raw.githubusercontent.com/mostafa-sultan/slums/main/data/images/md/" + content 
+     setContent(window.location.hash.split("/")[2]);
+    // console.log(window.location.hash.split("/")[2]);
+    const contentLink = "https://raw.githubusercontent.com/mostafa-sultan/slums/main/data/images/md/" + content
     axios.get(contentLink)
       .then(function (response) {
         setData(String(response.data));
       })
       .catch(function (error) {
         console.log(error);
-      });
+      }, [history]);
 
     console.log("data2")
 
